d11ae24a5e76a4a1d82a25b59f6ff89326d6e583
Vladimir <vkonovalov74@gmail.com>
2026-03-01 17:42:49 +0500

    Добавил аттачи

--- tests_hw/conftest.py
+++ tests_hw/conftest.py
@@ -1,10 +1,18 @@
 import pytest
-from selene import browser, have
+from selene import browser
+from utils import attach
+
+
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_setup():
     browser.config.driver_name = 'chrome'
 
     yield
+
+    attach.add_html(browser)
+    attach.add_logs(browser)
+    attach.add_screenshot(browser)
+    attach.add_video(browser)
 
     browser.quit()
