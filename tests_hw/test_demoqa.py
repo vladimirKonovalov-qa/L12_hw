59fab92a6a9ebf1f4e07cc94a4f4e64cfcb21038
Vladimir <vkonovalov74@gmail.com>
2026-03-01 17:36:44 +0500

    Добавил степы для allure

--- tests_hw/test_demoqa.py
+++ tests_hw/test_demoqa.py
@@ -9,11 +9,15 @@ from models.pages.registration_page import RegistrationPage
 def test_registration():
     vasya = User('Vasya', 'Zadov', 'ZadovVasya@pochta.org', 'Male', '9876567456', datetime.date(1974, 3, 3), 'Maths',
                  Hobbies.Reading, '230018.jpg', 'Boslhaya st, 19', 'NCR', 'Gurgaon')
-
     registration_page = RegistrationPage()
 
-    registration_page.open_started_page()
-    registration_page.register(vasya)
-    registration_page.should_have_registered(vasya)
+    with allure.step('Открываем страницу регистрации'):
+        registration_page.open_started_page()
+
+    with allure.step('Вводим данные пользователя'):
+        registration_page.register(vasya)
+
+    with allure.step('Проверяем корректность данных после регистрации в submit-форме'):
+        registration_page.should_have_registered(vasya)
 
 
