7fdb9e00a561150389c2722dcee46720dae8693c
Vladimir <vkonovalov74@gmail.com>
2026-03-01 17:53:20 +0500

    Добавил использование selenoid

--- tests_hw/conftest.py
+++ tests_hw/conftest.py
@@ -1,12 +1,35 @@
+import os
+
 import pytest
+from dotenv import load_dotenv
 from selene import browser
 from utils import attach
-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_setup():
-    browser.config.driver_name = 'chrome'
+    load_dotenv()
+
+    selenoid_login = os.getenv("LOGIN")
+    selenoid_pass = os.getenv("PASS")
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
 
     yield
 
